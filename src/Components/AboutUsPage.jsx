f7eb79365df159c1ccffcb30ec709acf461f82f4
TheVedant <vedantmute2004@gmail.com>
2023-12-22 13:06:02 +0530

    Day 6

--- src/Components/AboutUsPage.jsx
+++ src/Components/AboutUsPage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Title from "./Title";
-import '../Styles/AboutUsPage.css'
-import qualityImage1 from '../assets/about-best-cost.png'
-import qualityImage2 from '../assets/about-best-quality.png'
-import qualityImage3 from '../assets/about-best-rated.png'
+import "../Styles/AboutUsPage.css";
+import qualityImage1 from "../assets/about-best-cost.png";
+import qualityImage2 from "../assets/about-best-quality.png";
+import qualityImage3 from "../assets/about-best-rated.png";
 import aboutCompanyBanner from "../assets/about-banner.png";
 import director1 from "../assets/Director1.jpg";
 import director2 from "../assets/Director2.jpg";
@@ -14,15 +14,36 @@ export default function AboutUsPage() {
       <Title content="About Us" />
       <div className="about-company-container">
         <div className="about-company">
-          <div><h3><span className="yellow-text">INNOVATIVE</span> AND QUALITY DRIVEN COMPANY</h3>
-          <div className="about-company-info-para">
-          <p>Welcome to Sahyadri Developement constructors LLP(formerly known as Tarangan Associates), where a legacy of over two decades defines our commitment to excellence in construction. For more than 20 years, we've been shaping environments, exceeding expectations, and crafting enduring structures. Our highly skilled team, comprised of seasoned architects, engineers, and craftsmen, forms the heartbeat of SDC.
-    <br />
-At SDC, we pride ourselves on a diverse portfolio that spans residential and commercial developments, showcasing the depth of our capabilities. Our journey is marked by a dedication to innovation, integrity, and unwavering client satisfaction, earning us a stellar reputation in the construction landscape.
-<br />
-As we reflect on our 20+ years of service, our commitment to pushing the boundaries of construction remains unwavering. We embrace new technologies, set new standards for excellence, and invite you to join us on a journey where experience meets innovation.
-<br />
-          </p></div></div>
+          <div>
+            <h3>
+              <span className="yellow-text">INNOVATIVE</span> AND QUALITY DRIVEN
+              COMPANY
+            </h3>
+            <div className="about-company-info-para">
+              <p>
+                Welcome to Sahyadri Developement constructors LLP(formerly known
+                as Tarangan Associates), where a legacy of over two decades
+                defines our commitment to excellence in construction. For more
+                than 20 years, we've been shaping environments, exceeding
+                expectations, and crafting enduring structures. Our highly
+                skilled team, comprised of seasoned architects, engineers, and
+                craftsmen, forms the heartbeat of SDC.
+                <br />
+                At SDC, we pride ourselves on a diverse portfolio that spans
+                residential and commercial developments, showcasing the depth of
+                our capabilities. Our journey is marked by a dedication to
+                innovation, integrity, and unwavering client satisfaction,
+                earning us a stellar reputation in the construction landscape.
+                <br />
+                As we reflect on our 20+ years of service, our commitment to
+                pushing the boundaries of construction remains unwavering. We
+                embrace new technologies, set new standards for excellence, and
+                invite you to join us on a journey where experience meets
+                innovation.
+                <br />
+              </p>
+            </div>
+          </div>
         </div>
         {/* <div className="about-companny-banner">
           <img src={aboutCompanyBanner} alt="" />
@@ -30,47 +51,88 @@ As we reflect on our 20+ years of service, our commitment to pushing the boundar
       </div>
       <section className="quality-bar">
         <div className="quality">
-        <div className="quality-box">
-          <div className="quality-image"><img src={qualityImage1} alt="" /></div>
-          <div className="quality-details">
-          <div className="quality-heading">TOP RATED</div>
-          <div className="quality-info">On the other hand, we denounce with righteous indignation and dislike men who are</div>
+          <div className="quality-box">
+            <div className="quality-image">
+              <img src={qualityImage1} alt="" />
+            </div>
+            <div className="quality-details">
+              <div className="quality-heading">TOP RATED</div>
+              <div className="quality-info">
+                On the other hand, we denounce with righteous indignation and
+                dislike men who are
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="quality-box">
-          <div className="quality-image"><img src={qualityImage2} alt="" /></div>
-          <div className="quality-details">
-          <div className="quality-heading">BEST QUALITY</div>
-          <div className="quality-info">On the other hand, we denounce with righteous indignation and dislike men who are</div>
+          <div className="quality-box">
+            <div className="quality-image">
+              <img src={qualityImage2} alt="" />
+            </div>
+            <div className="quality-details">
+              <div className="quality-heading">BEST QUALITY</div>
+              <div className="quality-info">
+                On the other hand, we denounce with righteous indignation and
+                dislike men who are
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="quality-box">
-          <div className="quality-image"><img src={qualityImage3} alt="" /></div>
-          <div className="quality-details">
-          <div className="quality-heading">LOW COST</div>
-          <div className="quality-info">On the other hand, we denounce with righteous indignation and dislike men who are</div>
+          <div className="quality-box">
+            <div className="quality-image">
+              <img src={qualityImage3} alt="" />
+            </div>
+            <div className="quality-details">
+              <div className="quality-heading">LOW COST</div>
+              <div className="quality-info">
+                On the other hand, we denounce with righteous indignation and
+                dislike men who are
+              </div>
+            </div>
           </div>
-        </div>
         </div>
       </section>
-    
-    <h3><span className="yellow-text">Director's</span> Words of Wisdom</h3>
+
+      <h3>
+        <span className="yellow-text">Director's</span> Words of Wisdom
+      </h3>
 
       <div className="dirctor-message-container">
         <div className="directors-message">
-          <h4 className="director-name">Mr. Pravin Patel <span className="position">- Director</span> </h4><p>"In my capacity as Director at SDC, my foremost mission is to chart a course of excellence, originality, and client fulfillment. We are devoted to constructing not merely buildings but enduring spaces that transcend expectations. From our inaugural venture to each subsequent project, we aim to redefine our industry benchmarks, earning SDC a reputation for quality and innovation. I am committed to fostering a workplace culture that prizes the skills, creativity, and well-being of our team members."</p></div>
+          <h4 className="director-name">
+            Mr. Pravin Patel <span className="position">- Director</span>{" "}
+          </h4>
+          <p>
+            "In my capacity as Director at SDC, my foremost mission is to chart
+            a course of excellence, originality, and client fulfillment. We are
+            devoted to constructing not merely buildings but enduring spaces
+            that transcend expectations. From our inaugural venture to each
+            subsequent project, we aim to redefine our industry benchmarks,
+            earning SDC a reputation for quality and innovation. I am committed
+            to fostering a workplace culture that prizes the skills, creativity,
+            and well-being of our team members."
+          </p>
+        </div>
         <div className="directors-image">
           <img src={director1} alt="" />
         </div>
       </div>
       <div className="dirctor-message-container">
-      
         <div className="directors-message">
-          <h4 className="director-name">Mr. Mandar Mate<span className="position">- Director</span> </h4><p>"In my role as the Director at SDC, I am dedicated to steering our vision with a focus on ingenuity, collaboration, and client triumph. Our mission is to revolutionize construction norms, delivering influential projects that showcase our unwavering commitment. Together, we're crafting a lasting legacy of distinction, reshaping environments, and ensuring SDC stands as a beacon of inventive solutions and trust in the construction landscape. Let's embark on this journey of excellence."</p></div>
-          <div className="directors-image">
+          <h4 className="director-name">
+            Mr. Mandar Mate<span className="position">- Director</span>{" "}
+          </h4>
+          <p>
+            "In my role as the Director at SDC, I am dedicated to steering our
+            vision with a focus on ingenuity, collaboration, and client triumph.
+            Our mission is to revolutionize construction norms, delivering
+            influential projects that showcase our unwavering commitment.
+            Together, we're crafting a lasting legacy of distinction, reshaping
+            environments, and ensuring SDC stands as a beacon of inventive
+            solutions and trust in the construction landscape. Let's embark on
+            this journey of excellence."
+          </p>
+        </div>
+        <div className="directors-image">
           <img src={director2} alt="" />
         </div>
-        
       </div>
     </>
   );
--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,10 +1,83 @@
-import React from 'react'
-import Title from './Title'
+import React from "react";
+import Title from "./Title";
+import "../Styles/ContactUs.css";
+import map from "../assets/map.png";
+import phone from "../assets/phone.png";
+import email from "../assets/email.png";
+import location from "../assets/location.png";
+import businesscontact from "../assets/contact-banner.png";
+
+const ContactBox = ({ imageSrc, heading, info }) => {
+  return (
+    <div className="contact-box">
+      <div className="contact-image">
+        <img src={imageSrc} alt="" />
+      </div>
+      <div className="contact-details">
+        <div className="contact-heading">{heading}</div>
+        <div className="contact-info">{info}</div>
+      </div>
+    </div>
+  );
+};
 
 export default function ContactUs() {
   return (
     <>
       <Title content="Contact Us" />
+      <div className="contact-boxes">
+        <ContactBox
+          imageSrc={phone}
+          heading="Phone"
+          info="+91 99875 56874,  +91 99875 55877"
+        />
+        <ContactBox
+          imageSrc={email}
+          heading="Email"
+          info="admin@sahyadri.com"
+        />
+        <ContactBox
+          imageSrc={location}
+          heading="Location"
+          info="Flat No.803, Building B, Madhuvanti,
+        Sinhagad Road, Nandedcity,
+        Pune 411041."
+        />
+      </div>
+      <div className="business-contact">
+        <div className="business-contact-image">
+          <img src={businesscontact} alt="" />
+        </div>
+        <div className="business-contact-form">
+          <h3>
+            <span className="yellow-text">BUSINESS </span>ENQUIRY
+          </h3>
+          <div className="input">
+          <div id="textInput1">
+              <input type="text" placeholder="Enter Name" />
+            </div>
+            <div id="phoneInput">
+              <input type="tel" placeholder="Enter phone number" />
+            </div>
+
+            <div id="emailInput">
+              <input type="email" placeholder="Enter email address" />
+            </div>
+
+            
+
+            <div id="textInput2">
+              <input type="text" placeholder="Enter Message" />
+            </div>
+            <button type="submit" className="submitButton">Submit</button>
+          </div>
+        </div>
+      </div>
+      <div className="map">
+      {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.613508281609!2d73.78436311078141!3d18.455849308914182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc29598fdbfdc63%3A0x1b56e0dfe786b471!2sMadhuvanti%20Society!5e0!3m2!1sen!2sin!4v1703229941958!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
+      <img src={map} alt="" />
+</div>
+
     </>
-  )
+  );
 }
